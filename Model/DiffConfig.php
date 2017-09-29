525bf1dd64ca9c5710a4404883f1517e02ead382
Alexander Dite <a.dite@netz98.de>
2017-09-29 15:53:15 +0200

    [MAGEHACKMUC-17] add model,resourceModel and collection for the diff_config table

---
+++ Model/DiffConfig.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace Magenerds\SystemDiff\Model;
+
+/**
+ * Class DiffConfig
+ * @package Magenerds\SystemDiff\Model
+ */
+class DiffConfig extends \Magento\Framework\Model\AbstractModel
+{
+    /**
+     * Initialize resource
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(\Magenerds\SystemDiff\Model\ResourceModel\DiffConfig::class);
+    }
+
+}
